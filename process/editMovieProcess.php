dd54552fbd4a91b41591b5695c7782aa6c4e87b1
Novsada <NovsadaPhasa@gmail.com>
2022-09-22 22:49:56 +0700

    update folder Process

---
+++ process/editMovieProcess.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+if(isset($_POST['tambahMovie'])){
+    include ('../db.php');
+    $genre = $_POST['genre'];
+    $name = $_POST['name'];
+    $realese = $_POST['realese'];
+    $season = $_POST['season'];
+    $synopsis = $_POST['synopsis'];
+    $id = $_POST['id'];
+    $queryEdit = mysqli_query($con, "UPDATE movies SET genre='$genre', name='$name', realese='$realese', season='$season' WHERE id='$id'") or
+    die(mysqli_error($con));
+    if($queryEdit){
+        echo
+        '<script>
+        alert("Edit Success"); window.location = "../page/listMoviesPage.php"
+        </script>';
+    }else{
+        echo
+        '<script>
+        alert("Edit Failed"); window.location = "../page/listMoviesPage.php"
+        </script>';
+    }
+}else {
+    echo
+    '<script>
+    window.history.back()
+    </script>';
+}
+?>
--- process/registerProcess.php
+++ process/registerProcess.php
@@ -10,24 +10,35 @@
         $phonenum = $_POST['phonenum'];
         $membership = $_POST['membership'];
 
-        $query = mysqli_query($con,
-            "INSERT INTO users(email, password, name, phonenum, membership)
-                VALUES
-        ('$email', '$password', '$name', '$phonenum', '$membership')")
-             or die(mysqli_error($con)); 
-
-        if($query){
+        $queryCari = mysqli_query($con, "SELECT * FROM users WHERE email='$email'") or
+        die(mysqli_error($con));
+        if (mysqli_num_rows($queryCari) != 0) {
             echo
                 '<script>
-                alert("Register Success");
+                alert("Email Sudah Terdaftar");
                 window.location = "../index.php"
                 </script>';
         }else{
-            echo
-                '<script>
-                alert("Register Failed");
-                </script>';
+            $query = mysqli_query($con,
+                "INSERT INTO users(email, password, name, phonenum, membership)
+                    VALUES
+            ('$email', '$password', '$name', '$phonenum', '$membership')")
+                or die(mysqli_error($con)); 
+
+            if($query){
+                echo
+                    '<script>
+                    alert("Register Success");
+                    window.location = "../index.php"
+                    </script>';
+            }else{
+                echo
+                    '<script>
+                    alert("Register Failed");
+                    </script>';
+            }
         }
+        
     }else{
       echo
        '<script>
